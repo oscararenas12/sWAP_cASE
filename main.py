c7ffaeb1f98f7c4c509309883508ad486031e4e8
elo <oscararenas625@pm.me>
2023-09-28 21:50:50 -0700

    Goal: You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa. Finished less than 20min

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 def swap_case(s):
-    return
+    new_s = ''
+    for i in range(len(s)):
+        if s[i].isupper():
+            new_s += s[i].lower()
+        elif s[i].islower():
+            new_s += s[i].upper()
+        else:
+            new_s += s[i]
+    return new_s
 
 if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
